3e7a44ba656d03457a367bb81caa06d766529476
Mikhail Nefedev <nmwind@gmail.com>
2024-03-25 11:40:26 +0300

    config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,35 @@
+import { LocationStrategy, PathLocationStrategy } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { HashLocationStrategy, LocationStrategy, PathLocationStrategy } from '@angular/common';
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { AppLayoutModule } from './layout/app.layout.module';
+import { AppComponent } from './app.component';
 import { NotfoundComponent } from './demo/components/notfound/notfound.component';
-import { ProductService } from './demo/service/product.service';
 import { CountryService } from './demo/service/country.service';
 import { CustomerService } from './demo/service/customer.service';
 import { EventService } from './demo/service/event.service';
 import { IconService } from './demo/service/icon.service';
 import { NodeService } from './demo/service/node.service';
 import { PhotoService } from './demo/service/photo.service';
+import { ProductService } from './demo/service/product.service';
+import { AppLayoutModule } from './layout/app.layout.module';
 
 @NgModule({
-    declarations: [AppComponent, NotfoundComponent],
-    imports: [AppRoutingModule, AppLayoutModule],
+    declarations: [
+        AppComponent,
+        NotfoundComponent
+    ],
+    imports: [
+        AppRoutingModule,
+        AppLayoutModule
+    ],
     providers: [
-        { provide: LocationStrategy, useClass: PathLocationStrategy },
+        {
+            provide: LocationStrategy,
+            useClass: PathLocationStrategy
+        },
         CountryService, CustomerService, EventService, IconService, NodeService,
         PhotoService, ProductService
     ],
     bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+}
